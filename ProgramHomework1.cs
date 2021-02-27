a471aa1073bfffea10c407e4cce4a4caa4a53047
Luk716r <79133900+Luk716r@users.noreply.github.com>
2021-02-27 14:06:13 +0300

    Домашка

---
+++ ProgramHomework1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HomeWorkNumOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int RememberNum = 0;
+            bool t = false;
+            Console.WriteLine("Введите первое число последовательности больше нуля");
+            int NumOne = Int32.Parse(Console.ReadLine());
+            if (NumOne < 0)
+            {
+                Console.WriteLine("Ваше число меньше или равно нулю,введите другое");
+                NumOne = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Введите второе число");
+            int NumTwo = Int32.Parse(Console.ReadLine());
+            int min = NumOne + NumTwo;
+            while(t!= true)
+            {
+                //Console.WriteLine("Введите следующее число последовательности");
+                int Num_1 = Int32.Parse(Console.ReadLine());
+
+                if (Num_1 < 0)
+                {
+                    t = true;
+                    break;
+                }
+                int Num_2 = Int32.Parse(Console.ReadLine());
+                
+                if (Num_2 < 0)
+                {
+                    t = true;
+                    break;
+                }
+                RememberNum = Num_1 + Num_2;
+                if (min > RememberNum) 
+                {
+                    min = RememberNum;
+                }
+            }
+            Console.Write("Минимальная сумма   ");
+            Console.WriteLine(min);
+        }
+    }
+}
