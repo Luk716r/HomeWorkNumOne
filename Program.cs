f6ee3e819851bf53d65640cd06d60f6d147da6e2
Luk716r <79133900+Luk716r@users.noreply.github.com>
2021-02-28 14:29:07 +0300

    Add files via upload
    Дз2:
    Даны натуральные n и целые числа а1 и аn , найти кол-во четных членов последовательности.
    Для решения задачи необходимо две вспомогательных переменных.
    count счетчик введенных членов последовательности
    count счетчик четных членов последовательности
    а в цикле необходимо проверить очередное значение а и проверить его на четность

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HomeWorkNumTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = false;
+            int CounterNum = 0;
+            int EvenNumber = 0;
+            while(t != true)
+            {
+                int NaturalNumber = Int32.Parse(Console.ReadLine());
+                CounterNum++;
+                if(NaturalNumber<1)
+                {
+                    t = true;
+                    break;
+                }
+                if(NaturalNumber % 2 ==0)
+                {
+                    EvenNumber++;
+                }
+               
+            }
+            Console.WriteLine(CounterNum);
+            Console.WriteLine(EvenNumber);
+
+        }
+    }
+}
